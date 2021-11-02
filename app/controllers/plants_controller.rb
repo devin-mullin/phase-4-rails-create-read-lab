e5647f241ea52da76b357fc3cc4de7060ae19235
Devin Mullin <devin.mullin90@gmail.com>
2021-11-02 11:19:38 -0500

    commit tree 1caaa1b3089822f7705858e7cbe797bee8952bbb

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,18 @@
+class PlantsController < ApplicationController
+
+    def index
+        plant = Plant.all
+        render json: plant
+    end
+
+    def show
+        plant = Plant.find_by_id(params[:id])
+        render json: plant
+    end
+
+    def create
+        plant = Plant.create(name: params[:name], image: params[:image], price: params[:price])
+        render json: plant, status: :created
+    end
+
+end
